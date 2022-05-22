586de2b119dd469b951bfc06436e2d4819a10f0c
Your Namefurkasf <furkantural3328@hotmail.com>
2022-05-22 19:35:59 +0300

    some directory reorginize by featurs and case write for level conroler if player pass max level

--- Assets/Scripts/SaveAndSettings/LevelController.cs
+++ Assets/Scripts/SaveAndSettings/LevelController.cs
@@ -8,13 +8,28 @@ public class LevelController : MonoBehaviour
     public GameSettings gameSettings;
     public Transform Player;
     int level = 0;
+    int maxLevel = 3;
 
     public void NextLevel()
     {
+        if (level < maxLevel)
+        {
+            Destroy(GameObject.Find("TheLevelContailner").transform.GetChild(0).gameObject);
+            level++;
+            Instantiate(gameSettings.LevelPrefabs[level].gameObject, GameObject.Find("TheLevelContailner").transform);
+            Player.position = gameSettings.playerDefaultPos;
+            UI.SetActive(false);
+
+        }
+        else
+        {
+            //game open wictory panel 
+        }
+    }
+    public void RestartLevel()
+    {
         Destroy(GameObject.Find("TheLevelContailner").transform.GetChild(0).gameObject);
-        level++;
         Instantiate(gameSettings.LevelPrefabs[level].gameObject, GameObject.Find("TheLevelContailner").transform);
-        Player.position = gameSettings.playerDefaultPos;
         UI.SetActive(false);
     }
 }
