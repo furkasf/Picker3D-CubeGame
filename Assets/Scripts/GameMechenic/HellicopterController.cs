e2bc8c3cce470595d8bf8a4fd406f65a3d9dfb64
Your Namefurkasf <furkantural3328@hotmail.com>
2022-05-25 17:29:15 +0300

    helicopter mechnanin animations added

--- Assets/Scripts/GameMechenic/HellicopterController.cs
+++ Assets/Scripts/GameMechenic/HellicopterController.cs
@@ -5,45 +5,56 @@ using UnityEngine;
 
 public class HellicopterController : MonoBehaviour
 {
-    [SerializeField] Transform PlayerPosition;
-    [SerializeField] Transform Planor;
-    [SerializeField] float animationDuration = 1f;
-    [SerializeField] GameObject ball;
-    Sequence seq;
-    bool animationTrigger = false;
+    [SerializeField] Transform _target;
+    [SerializeField] float _duration;
+    [SerializeField] Transform _planor;
+    [SerializeField] GameObject _BigballPrefab;
+
+    private void Awake()
+    {
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     void Start()
     {
-        PlayerPosition = GameObject.FindGameObjectWithTag("Player").transform;
-        Planor.DORotate(new Vector3(0, 360, 0), animationDuration * 0.5f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        _planor.DORotate(new Vector3(0, 360, 0), _duration * 0.5f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart);
+        _duration = 1.5f;
     }
 
 
     void Update()
     {
 
-        if (Vector3.Distance(PlayerPosition.position, transform.position) < 5f)
+        if (Vector3.Distance(_target.position, transform.position) < 10f)
         {
-            animationTrigger = true;
+            StartCoroutine(SyncAnimations());
+        }
 
-        }
-        if(animationTrigger)
-        {
-            Invoke("DropBalls", 1.5f);
-            BallPoolController.instance.GetBallFromPool();
-            seq = DOTween.Sequence();
-            seq.Append(transform.DOMove(new Vector3(0 ,0, 6), animationDuration))
-                .Append(transform.DOMove(new Vector3(4, 0, 0), animationDuration))
-                .Append(transform.DOMove(new Vector3(0, 0, 13), animationDuration))
-                .Append(transform.DOMove(new Vector3(0, 10, 0), animationDuration)).SetEase(Ease.InSine).OnComplete(() => gameObject.SetActive(false));
-        }
-        
     }
 
-   void DropBalls()
+    void SpawnBalls()
     {
         GameObject ball = BallPoolController.instance.GetBallFromPool();
-        ball.transform.position = transform.position;
+        //set as child of parent
+        ball.transform.parent = transform.parent;
+        ball.transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
+        if (ball.transform.parent.tag != "BallContainer")
+        {
+            //after balls relocated rechange parent to pool container
+            ball.transform.parent = GameObject.FindGameObjectWithTag("BallContainer").transform;
+        }
+    }
+
+    IEnumerator SyncAnimations()
+    {
+        Invoke("SpawnBalls", _duration * 0.5f);
+        transform.DOLocalMove(new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + 10), _duration).SetEase(Ease.Linear);
+        yield return new WaitForSecondsRealtime(_duration);
+        Invoke("SpawnBalls", _duration * 0.5f);
+        transform.DOLocalMove(new Vector3(transform.localPosition.x + 6.5f, transform.localPosition.y, transform.localPosition.z), _duration).SetEase(Ease.Linear);
+        yield return new WaitForSecondsRealtime(_duration);
+        transform.DOLocalMove(new Vector3(transform.localPosition.x , transform.localPosition.y + 20, transform.localPosition.z), _duration).SetEase(Ease.Linear).OnComplete(() => gameObject.SetActive(false));
+        yield return null;
     }
 }
   
